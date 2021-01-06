b6cb6b156af6599c71ea0678a5367a7ccc23371a
erikanni <erik.anni@gmail.com>
2021-01-06 18:04:37 -0500

    add radian, degree, and revolution conversions

--- rotational-kinematics.py
+++ rotational-kinematics.py
@@ -1,1 +1,28 @@
 import math
+
+def solve():
+    choice = int(input("enter 1 for rev, 2 for deg, or 3 for rad"))
+    if choice == 1:
+        rev = float(input("enter rev"))
+        deg = rev * 360
+        rad = deg * math.pi/180
+        print("deg: " + str(deg))
+        print("rad: " + str(rad))
+
+    if choice == 2:
+        deg = float(input("enter degrees"))
+        rev = deg/360
+        rad = deg*math.pi/180
+        print("rev: " + str(rev))
+        print("rad: " + str(rad))#TODO
+
+    if choice == 3:
+        rad = float((input("enter rad")))
+        deg = rad*180/math.pi
+        rev = deg/360 
+        print("deg: " + str(deg))
+        print("rev: " + str(rev))
+
+
+
+solve()
