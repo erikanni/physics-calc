8665a95825bf628a576f7f6e8446f3a8a2754539
erikanni <erik.anni@gmail.com>
2021-01-13 16:20:56 -0500

    add prototype for new menu

--- force-calc.py
+++ force-calc.py
@@ -35,6 +35,23 @@ def main():
     solve()
 
 
+def func_d():
+    print("func d")
+
+#def main():
+    print('''
+    function a
+    function b
+    function c
+    function d
+    ''')
+    menu_dict = {"a": func_a, "b": func_b, "c": func_c, "d": func_d}
+    answer = input("select function: ").lower()[0]
+    if answer in menu_dict:
+        menu_dict[answer]()
+    else:
+        print("No such function")
+
 if __name__ == '__main__':
     main()
 
